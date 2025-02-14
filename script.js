4ce562ecf839bd20b89bc773b01aa1eccc229db1
Virgínia Colares <vmcolares@inf.ufsm.br>
2025-02-14 14:25:38 -0300

    Responsividade e ajustes

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Função de rolagem suave com offset
-    function smoothScroll(target) {
-      const element = document.querySelector(target);
-      const offset = 100; // Ajuste esse valor conforme necessário para o seu layout (pode ser -50, -120, etc.)
-      
-      window.scrollTo({
-        top: element.offsetTop - offset, // Aplica o deslocamento
-        behavior: 'smooth'
-      });
-    }
-  
-    // Adiciona o evento de clique nos links de navegação
-    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-      anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        smoothScroll(this.getAttribute('href'));
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  function smoothScroll(target) {
+    const element = document.querySelector(target);
+    const offset = 100;
+
+    window.scrollTo({
+      top: element.offsetTop - offset,
+      behavior: 'smooth'
     });
-  
-    // Ação para o botão "Ver sobre mim"
-    document.getElementById('ver-sobre-mim').addEventListener('click', function() {
-      smoothScroll('#sobre');
-    });
-  
-    // Ação para o nome Virginia Colares
-    document.getElementById('virginia-colares').addEventListener('click', function() {
-      smoothScroll('#hero');
+  }
+
+  document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      smoothScroll(this.getAttribute('href'));
     });
   });
-  
+
+  document.getElementById('ver-sobre-mim').addEventListener('click', function () {
+    smoothScroll('#sobre');
+  });
+
+  document.getElementById('virginia-colares').addEventListener('click', function () {
+    smoothScroll('#hero');
+  });
+
+  const navbarToggle = document.querySelector('.navbar-toggle');
+  const navbarMenu = document.querySelector('.navbar-menu');
+
+  if (navbarToggle) {
+    navbarToggle.addEventListener('click', function () {
+      navbarMenu.classList.toggle('show');
+    });
+  }
+});
